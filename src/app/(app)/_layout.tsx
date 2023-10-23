f386275fd8f8ad9ffd1412f92f96af01e7b950b9
Allyson-Santana <santana.allyson459@gmail.com>
2023-10-22 23:11:06 -0300

    wip

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -4,79 +4,75 @@ import {
     MaterialIcons,
     FontAwesome,
     AntDesign
-} from '@expo/vector-icons';
-import { Text } from 'react-native';
+} from "@expo/vector-icons";
+import { Text } from "react-native";
+import { useSession } from "@/ctx";
 
-import { auth } from "@/config/firebaseConfig";
-import { onAuthStateChanged } from "firebase/auth";
-
-const TabRoutersLayout = () => (
-    <Tabs screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: "#755843",
-    }}>
-        <Tabs.Screen
-            name="home"
-            options={{
-                tabBarLabel: "Home",
-                tabBarIcon: ({ color, size }) => (
-                    <AntDesign name="home" size={size} color={color} />
-                )
-            }}
-        />
-        <Tabs.Screen
-            name="service"
-            options={{
-                // href: "/service",
-                // title: "Service",
-                tabBarLabel: "Service",
-                tabBarIcon: ({ color, size }) => (
-                    <FontAwesome name="calendar" size={size} color={color} />
-                )
-            }}
-        />
-
-        <Tabs.Screen
-            name="profile"
-            options={{
-                tabBarLabel: "Profile",
-                tabBarIcon: ({ color, size }) => (
-                    <MaterialIcons name="person" size={size} color={color} />
-                )
-            }}
-        />
-    </Tabs>
-)
+//     // onAuthStateChanged(auth, (user) => {
+//     //     if (user) {
+//     //         // User is signed in, see docs for a list of available properties
+//     //         // https://firebase.google.com/docs/reference/js/auth.user
+//     //         const uid = user.uid;
+//     //         // ...
+//     //     } else {
+//     //         // User is signed out
+//     //         // ...
+//     //     }
+//     // });
+    
 
 export default function AppLayout() {
+    const { session, isLoading } = useSession();
 
-    // onAuthStateChanged(auth, (user) => {
-    //     if (user) {
-    //         // User is signed in, see docs for a list of available properties
-    //         // https://firebase.google.com/docs/reference/js/auth.user
-    //         const uid = user.uid;
-    //         // ...
-    //     } else {
-    //         // User is signed out
-    //         // ...
-    //     }
-    // });
-    
-    // const { session, isLoading } = auth();
+    // You can keep the splash screen open, or render a loading screen like we do here.
+    if (isLoading) {
+        return <Text>Loading...</Text>;
+    }
 
-    // // You can keep the splash screen open, or render a loading screen like we do here.
-    // if (isLoading) {
-    //     return <Text>Loading...</Text>;
-    // }
+    // Only require authentication within the (app) group's layout as users
+    // need to be able to access the (auth) group and sign in again.
+    if (!session) {
+        // On web, static rendering will stop here as the user is not authenticated
+        // in the headless Node process that the pages are rendered in.
+        return <Redirect href="/sign-in" />;
+    }
 
-    // // Only require authentication within the (app) group's layout as users
-    // // need to be able to access the (auth) group and sign in again.
-    // if (!session) {
-    //     // On web, static rendering will stop here as the user is not authenticated
-    //     // in the headless Node process that the pages are rendered in.
-    //     return <Redirect href="/sign-in" />;
-    // }
+    // This layout can be deferred because it's not the root layout.
+    return (
+        <Tabs screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: "#755843",
+        }}>
+            <Tabs.Screen
+                name="home"
+                options={{
+                    tabBarLabel: "Home",
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="home" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="service"
+                options={{
+                    // href: "/service",
+                    // title: "Service",
+                    tabBarLabel: "Service",
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="calendar" size={size} color={color} />
+                    )
+                }}
+            />
 
-    // // This layout can be deferred because it's not the root layout.
-    return <TabRoutersLayout />;
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    tabBarLabel: "Profile",
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons name="person" size={size} color={color} />
+                    )
+                }}
+            />
+        </Tabs>
+    )
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,36 +1,49 @@
 
-import { Tabs, Redirect, Stack } from "expo-router";
-import {
-    MaterialIcons,
-    FontAwesome,
-    AntDesign
-} from '@expo/vector-icons';
-import { Text, } from 'react-native';
+// import { Tabs, Redirect, Stack } from "expo-router";
+// import {
+//     MaterialIcons,
+//     FontAwesome,
+//     AntDesign
+// } from '@expo/vector-icons';
+// import { Text, } from 'react-native';
 
-import { auth } from "@/config/firebaseConfig";
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
+// import { auth } from "@/config/firebaseConfig";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { useEffect } from "react";
 
 
-export default function AppLayout() {
+// export default function AppLayout() {
 
-    useEffect(() => {
-        const subscriber = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                // User is signed in, see docs for a list of available properties
-                // https://firebase.google.com/docs/reference/js/auth.user
-                const uid = user.uid;
-                // ...
-                console.log(user) 
-            } else {
-                // User is signed out
-                // ...
-            }
-        });
+//     useEffect(() => {
+//         const subscriber = onAuthStateChanged(auth, (user) => {
+//             if (user) {
+//                 // User is signed in, see docs for a list of available properties
+//                 // https://firebase.google.com/docs/reference/js/auth.user
+//                 const uid = user.uid;
+//                 // ...
+//                 console.log(user) 
+//             } else {
+//                 // User is signed out
+//                 // ...
+//             }
+//         });
 
-        return subscriber;
-    },[])
+//         return subscriber;
+//     },[])
 
-    return <Stack />
+//     return <Stack />
 
+// }
+
+import { Slot } from 'expo-router';
+import { SessionProvider } from '@/ctx';
+
+export default function Root() {
+    // Set up the auth context and render our layout inside of it.
+    return (
+        <SessionProvider>
+            <Slot />
+        </SessionProvider>
+    );
 }
+
